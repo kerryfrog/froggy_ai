31b473ff34d35e68f9461ebd1a65d3da5312c49b
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-11-06 22:06:12 +0900

    대용량 pkl file lfs로 업로드

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_restful import reqparse
-
+import joblib
 
 app = Flask(__name__)
 
@@ -17,11 +17,14 @@ def predict():
     parser = reqparse.RequestParser()
     data = {
         'test' : 'get data from flask server!',
-        'som':'dasom'
+        'som' : 'dasom'
     }
     return jsonify(data)
 
 
 # 파이썬 명령어로 실행할 수 있음
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    cf = joblib.load("./cf_factorization.pkl")
+    mtrx = cf.get_complete_matrix()
+    # mtrx.summary()
+    app.run(host='127.0.0.1', port=8000, debug=True)
