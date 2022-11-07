0628f1c707143ae4d36d78f9e74e94dd8b4bcc7e
이예은, Ye Eun Lee <lena0323park@naver.com>
2022-11-07 19:37:46 +0900

    json parse

--- app.py
+++ app.py
@@ -4,27 +4,44 @@ import joblib
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 
 @app.route('/recommend/pattern')
 def pattern():
     return "ppatternRouter"
 
+
 @app.route('/recommend', methods=['POST'])
 def predict():
     parser = reqparse.RequestParser()
     data = {
-        'test' : 'get data from flask server!',
-        'som' : 'dasom'
+        'test': 'get data from flask server!',
+        'som': 'dasom'
     }
     return jsonify(data)
 
 
+@app.route('/newmodel', methods=['POST'])
+def addRow():
+    try:
+        parser = reqparse.RequestParser()
+        parser.add_argument('targetId', type=str)
+        parser.add_argument('userId', type=str)
+        args = parser.parse_args()
+
+        _targetId = args['targetId']
+        _userId = args['userId']
+        return {'TargetId': args[_targetId], 'UserId': args['userId']}
+
+    except Exception as e:
+        return {'error': str(e)}
+
+
 # 파이썬 명령어로 실행할 수 있음
 if __name__ == '__main__':
-    cf = joblib.load("./cf_factorization.pkl")
-    mtrx = cf.get_complete_matrix()
     # mtrx.summary()
     app.run(host='127.0.0.1', port=8000, debug=True)
