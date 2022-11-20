1424e02de5773a3aaec9cccd3768d221a2e6b063
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2022-11-20 19:35:58 +0900

    change app host

--- main.py
+++ main.py
@@ -311,4 +311,4 @@ def parse():
 # 파이썬 명령어로 실행할 수 있음
 if __name__ == '__main__':
     # mtrx.summary()
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
